8ab7dc2004dd3c75428038aa7844df8e7063a710
Israela608 <israela608@gmail.com>
2025-07-05 02:04:31 +0100

    Lazy Column

--- app/src/main/java/com/example/newsinshort/ui/components/AppComponents.kt
+++ app/src/main/java/com/example/newsinshort/ui/components/AppComponents.kt
@@ -5,12 +5,20 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.newsinshort.data.entity.NewsResponse
 import com.example.newsinshort.ui.theme.Purple40
 
 @Composable
@@ -28,4 +36,29 @@ fun Loader(){
             color = Purple40,
         )
     }
+}
+
+@Composable
+fun NewsList(response: NewsResponse){
+    LazyColumn {
+        items(response.articles){
+            article ->
+            NormalTextComponent(textValue = article.title ?: "NA")
+        }
+    }
+}
+
+@Composable
+fun NormalTextComponent(textValue: String){
+    Text(
+        text = textValue,
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentHeight()
+            .padding(8.dp),
+        style = TextStyle(
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Normal,
+        )
+    )
 }
